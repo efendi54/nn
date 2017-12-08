daeb43618fe941cf45d8828d743db94609492091
Ayhan Ueretmen <aueret@googlemail.com>
2017-12-08 10:07:17 +0100

    extended header documentation

--- include/net/layer.h
+++ include/net/layer.h
@@ -18,17 +18,16 @@ struct Layer
   void Feed(const DVec &p_X);
   void Print(void);
 
-  DVec t_In;
-  DVec t_Out;
-  DVec t_DOut;
-  DVec t_Err;
-  DVec t_Bias;
-  DVec t_BiasDelta;
+  DVec t_In;        ///< all neurons inputs
+  DVec t_Out;       ///< all neurons outputs
+  DVec t_DOut;      ///< all neurons outputs passed in through derivate out-function (see also t_Func)
+  DVec t_Err;       ///< all neurons errors
+  DVec t_Bias;      ///< all neurons biases
+  DVec t_BiasDelta; ///< deltas for all biases for recalculation of deltas during learning 
+  Func t_Func;      ///< function to be used for calculating neuron outputs
+  Func t_DFunc;     ///< derivate function of t_Func to be used for calculating derivate-outputs
 
-  Func t_Func;
-  Func t_DFunc;
-
-  std::vector<DVec> t_Weights;
-  std::vector<DVec> t_WeightDeltas;
+  std::vector<DVec> t_Weights;      ///< weighted connections from this layer to previous layer (in direction input to output)
+  std::vector<DVec> t_WeightDeltas; ///< deltas for recalculation of new weights during learning
 };
 
